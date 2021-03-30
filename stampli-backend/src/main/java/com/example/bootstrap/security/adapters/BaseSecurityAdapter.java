2c420ab6fcb0d7e113a4f3d4f2158b9138a92bd3
Piotr Suwała <piotr.suwala@respublica.org.pl>
2021-03-30 20:06:21 +0200

    fighting with reverse proxy

--- stampli-backend/src/main/java/com/example/bootstrap/security/adapters/BaseSecurityAdapter.java
+++ stampli-backend/src/main/java/com/example/bootstrap/security/adapters/BaseSecurityAdapter.java
@@ -2,6 +2,8 @@ package com.example.bootstrap.security.adapters;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.header.HeaderWriterFilter;
+import org.springframework.web.filter.ForwardedHeaderFilter;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -9,6 +11,7 @@ public class BaseSecurityAdapter extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         final HttpSecurity output = http
+                .addFilterBefore(new ForwardedHeaderFilter(), HeaderWriterFilter.class)
                 .formLogin(login -> login.loginPage("/login"))
                 .oauth2Login(withDefaults());
         super.configure(output);
