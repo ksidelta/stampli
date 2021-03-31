c7c8d23a05e374a3c6993436a5e62a24e3bb0d8a
Piotr Suwała <piotr.suwala@respublica.org.pl>
2021-03-31 15:21:17 +0200

    added successful login page

--- stampli-backend/src/main/java/com/example/bootstrap/security/adapters/BaseSecurityAdapter.java
+++ stampli-backend/src/main/java/com/example/bootstrap/security/adapters/BaseSecurityAdapter.java
@@ -4,7 +4,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.context.request.async.WebAsyncManagerIntegrationFilter;
-import org.springframework.security.web.header.HeaderWriterFilter;
 import org.springframework.web.filter.ForwardedHeaderFilter;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -13,6 +12,7 @@ public class BaseSecurityAdapter extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         final HttpSecurity output = http
+                .formLogin(x -> x.successForwardUrl("/test/string"))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
                 .addFilterBefore(new ForwardedHeaderFilter(), WebAsyncManagerIntegrationFilter.class)
                 .oauth2Login(withDefaults());
