5f775b7de6389b39fb7f0aa3e196fab1d4076d8f
Piotr Suwała <piotr.suwala@respublica.org.pl>
2021-03-31 14:33:30 +0200

    added Forward filter before all

--- stampli-backend/src/main/java/com/example/bootstrap/security/adapters/BaseSecurityAdapter.java
+++ stampli-backend/src/main/java/com/example/bootstrap/security/adapters/BaseSecurityAdapter.java
@@ -2,6 +2,7 @@ package com.example.bootstrap.security.adapters;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.context.request.async.WebAsyncManagerIntegrationFilter;
 import org.springframework.security.web.header.HeaderWriterFilter;
 import org.springframework.web.filter.ForwardedHeaderFilter;
 
@@ -11,7 +12,7 @@ public class BaseSecurityAdapter extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         final HttpSecurity output = http
-                .addFilterBefore(new ForwardedHeaderFilter(), HeaderWriterFilter.class)
+                .addFilterBefore(new ForwardedHeaderFilter(), WebAsyncManagerIntegrationFilter.class)
                 .oauth2Login(withDefaults());
         super.configure(output);
     }
