cb013b781c325754a40d8edea676f0a4177bc315
Piotr Suwała <piotr.suwala@respublica.org.pl>
2021-04-04 21:06:57 +0200

    backend: added spin up of mariadb docker on integration-test

--- stampli-backend/src/test/java/com/example/configuration/DatabaseConfiguration.java
+++ stampli-backend/src/test/java/com/example/configuration/DatabaseConfiguration.java
@@ -2,20 +2,22 @@ package com.example.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.core.env.StandardEnvironment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
 
 @Configuration
 public class DatabaseConfiguration {
+
     @Primary
-    public DataSource dataSource() {
+    public DataSource dataSource(StandardEnvironment env) {
         final var source = new DriverManagerDataSource();
 
-        source.setUrl("jdbc:h2:mem:testdb");
-        source.setDriverClassName(org.h2.Driver.class.getName());
-        source.setUsername("sa");
-        source.setPassword("sa");
+        source.setUrl("jdbc:mariadb://localhost:" + env.getRequiredProperty("test.mysql.port") + "/" + env.getRequiredProperty("test.mysql.db"));
+        source.setDriverClassName(org.mariadb.jdbc.Driver.class.getName());
+        source.setUsername(env.getRequiredProperty("test.mysql.username"));
+        source.setPassword(env.getRequiredProperty("test.mysql.password"));
 
         return source;
     }
