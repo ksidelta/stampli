ca59fae3b7463b03bf3b3b4a493a3efdbbb5176a
Piotr Suwała <piotr.suwala@respublica.org.pl>
2021-05-22 20:30:55 +0200

    fixed ffucked up transaction

--- stampli-backend/src/test/java/com/example/controller/authentication/business/BusinessControllerE2E.java
+++ stampli-backend/src/test/java/com/example/controller/authentication/business/BusinessControllerE2E.java
@@ -1,6 +1,7 @@
 package com.example.controller.authentication.business;
 
 import com.example.BaseTestConfiguration;
+import com.example.common.db.AbstractDatabaseTest;
 import com.example.controller.business.BusinessController;
 import com.example.infrastructure.jwt.generator.TokenGenerator;
 import com.example.domain.context.authentication.user.entity.UserEntity;
@@ -17,6 +18,7 @@ import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
 import org.springframework.test.web.servlet.MockMvc;
 
 import javax.servlet.Filter;
+import javax.transaction.Transactional;
 import java.util.Arrays;
 
 import static org.hamcrest.CoreMatchers.equalTo;
@@ -30,7 +32,8 @@ import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standal
 
 @ExtendWith(SpringExtension.class)
 @SpringJUnitWebConfig({BaseTestConfiguration.class})
-public class BusinessControllerE2E {
+@Transactional
+public class BusinessControllerE2E extends AbstractDatabaseTest {
     protected MockMvc mockMvc;
 
     @Autowired
@@ -67,35 +70,25 @@ public class BusinessControllerE2E {
         final var business = BusinessAggregate.createBusinessAggregate(new Owner(1));
         businessRepository.save(business);
 
-        try {
-            mockMvc.perform(
-                    get("/api/business/")
-                            .accept(MediaType.APPLICATION_JSON)
-                            .contentType(MediaType.APPLICATION_JSON)
-                            .header("Authorization", "Bearer " + createToken())
-            ).andExpect(status().is(200))
-                    .andExpect(jsonPath("$.id", equalTo(business.getId())))
-                    .andExpect(jsonPath("$.name", equalTo(business.usingBusinessProfile().getBusinessName().getName())))
-                    .andReturn();
-
-        } finally {
-            businessRepository.remove(business);
-        }
+        mockMvc.perform(
+                get("/api/business/")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("Authorization", "Bearer " + createToken())
+        ).andExpect(status().is(200))
+                .andExpect(jsonPath("$.id", equalTo(business.getId())))
+                .andExpect(jsonPath("$.name", equalTo(business.usingBusinessProfile().getBusinessName().getName())))
+                .andReturn();
     }
 
     @Test
     public void givenNonExistingBusinessWhenCreatedThenReturnsCorrect() throws Exception {
-        try {
-            mockMvc.perform(
-                    post("/api/business/")
-                            .accept(MediaType.APPLICATION_JSON)
-                            .contentType(MediaType.APPLICATION_JSON)
-                            .header("Authorization", "Bearer " + createToken())
-            ).andExpect(status().is(201)).andReturn();
-
-        } finally {
-            businessRepository.remove(businessRepository.findByOwnerId(1).get());
-        }
+        mockMvc.perform(
+                post("/api/business/")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("Authorization", "Bearer " + createToken())
+        ).andExpect(status().is(201)).andReturn();
     }
 
     public String createToken() {
