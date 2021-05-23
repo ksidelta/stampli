f4522dd1d53e56466f7301ae301ffab2a5160da3
Piotr Suwała <piotr.suwala@respublica.org.pl>
2021-05-23 13:32:43 +0200

    added chokidar

---
+++ stampli-backend/src/main/java/com/example/controller/stamps/challenge/ChallengeController.java
@@ -0,0 +1,12 @@
+package com.example.controller.stamps.challenge;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping(path = "/api/business/challenge/{businessId}")
+public class ChallengeController {
+    public void createChallenge() {
+    }
+
+    public void claimChallenge() {
+    }
+}
