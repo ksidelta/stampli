243aba53e7c006fb4154b6227b9d9abab4c52ea5
Piotr Suwała <piotr.suwala@respublica.org.pl>
2021-06-02 16:03:46 +0200

    fixed stuff

--- stampli-backend/src/main/java/com/example/infrastructure/jms/JmsConfiguration.java
+++ stampli-backend/src/main/java/com/example/infrastructure/jms/JmsConfiguration.java
@@ -1,6 +1,7 @@
 package com.example.infrastructure.jms;
 
 import com.rabbitmq.jms.admin.RMQConnectionFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.core.JmsTemplate;
@@ -9,13 +10,16 @@ import javax.jms.ConnectionFactory;
 
 @Configuration
 public class JmsConfiguration {
+    @Value("#{systemProperties['rabbitmq.host']}")
+    protected String host;
+
     @Bean
     public ConnectionFactory connectionFactory() {
         RMQConnectionFactory connectionFactory = new RMQConnectionFactory();
         connectionFactory.setUsername("guest");
         connectionFactory.setPassword("guest");
         connectionFactory.setVirtualHost("/");
-        connectionFactory.setHost("localhost");
+        connectionFactory.setHost(host);
         connectionFactory.setPort(5672);
         return connectionFactory;
     }
