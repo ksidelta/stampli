e0c000e69efd4e512cada1b452cc14e722ab3aa7
Piotr Suwała <piotr.suwala@respublica.org.pl>
2021-06-13 16:58:30 +0200

    fixed tests for stamps

--- stampli-backend/src/main/java/com/example/infrastructure/db/DbTestUtil.java
+++ stampli-backend/src/main/java/com/example/infrastructure/db/DbTestUtil.java
@@ -1,14 +1,23 @@
 package com.example.infrastructure.db;
 
-import com.example.domain.context.stamps.command.StampClientAggregate;
 import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
+import org.springframework.core.type.filter.AnnotationTypeFilter;
+import org.springframework.core.type.filter.RegexPatternTypeFilter;
 import org.springframework.stereotype.Component;
 
+import javax.persistence.Entity;
 import javax.transaction.Transactional;
+import java.util.Set;
+import java.util.function.Consumer;
+import java.util.logging.Logger;
+import java.util.regex.Pattern;
 
 @Component
 @Transactional
 public class DbTestUtil {
+    Logger logger = Logger.getLogger(DbTestUtil.class.getName());
 
     SessionFactory sessionFactory;
 
@@ -17,8 +26,31 @@ public class DbTestUtil {
     }
 
     public void removeAll() {
+        forEachAggregateClass(this::purge);
+    }
+
+    protected void forEachAggregateClass(Consumer<Class<?>> consumer) {
+        final ClassPathScanningCandidateComponentProvider scanner = new ClassPathScanningCandidateComponentProvider(true);
+        scanner.addIncludeFilter(new RegexPatternTypeFilter(Pattern.compile(".*Aggregate.*")));
+        // scanner.addIncludeFilter(new AnnotationTypeFilter(Entity.class)); //
+
+        final Set<BeanDefinition> beans = scanner.findCandidateComponents("com.example.domain");
+
+        beans.forEach(bean -> {
+            try {
+                consumer.accept(Class.forName(bean.getBeanClassName()));
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
+
+    protected void purge(Class<?> klass) {
+        final var className = klass.getSimpleName();
+
         sessionFactory.getCurrentSession()
-                .createQuery("SELECT e FROM StampClientAggregate e", StampClientAggregate.class)
+                .createQuery("SELECT e FROM " + className + " e", klass)
                 .getResultStream()
                 .forEach(entity -> sessionFactory.getCurrentSession().delete(entity));
     }
