cfb1da8a10d1a3e5c5cd70dbf9ac3adf9003550a
psuwala <oneat@vp.pl>
2021-06-28 22:05:00 +0200

    backend: fixed stuff for spring boot

---
+++ stampli-backend/src/main/java/com/example/SpringBoot.java
@@ -0,0 +1,9 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+
+public class SpringBoot {
+    static public void main(String[] args) throws Exception {
+        SpringApplication.main(args);
+    }
+}
