6d45222a5630e7a883e7b05193f7e588450127c4
psuwala <oneat@vp.pl>
2021-06-29 22:12:58 +0200

    frontend: resorted deps + added correct token for backend

--- stampli-frontend/src/components/simple/buttons/login/GoogleLoginButton.tsx
+++ stampli-frontend/src/components/simple/buttons/login/GoogleLoginButton.tsx
@@ -3,19 +3,20 @@ import React from 'react';
 import styled from 'styled-components';
 import { OpenIdConfiguration } from '../../../../services/config/parts/OpenIdConfiguration';
 
-export const GoogleLoginButton = ({
-  config,
-  onSuccess,
-  onFailure = console.error
-}: {
-  config: OpenIdConfiguration;
-  onSuccess: (token: string) => void;
-  onFailure?: (error: string) => void;
-}) => (
+export const GoogleLoginButton = (
+  {
+    config,
+    onSuccess,
+    onFailure = console.error
+  }: {
+    config: OpenIdConfiguration;
+    onSuccess: (token: string) => void;
+    onFailure?: (error: string) => void;
+  }) => (
   <StyledGoogleLogin
     clientId={config.googleClientId}
-    buttonText="Login"
-    onSuccess={x => ('accessToken' in x ? onSuccess((x as GoogleLoginResponse).accessToken) : onFailure('Offline!'))}
+    buttonText='Login'
+    onSuccess={x => ('accessToken' in x ? onSuccess((x as GoogleLoginResponse).tokenId) : onFailure('Offline!'))}
     onFailure={x => onFailure(x)}
     cookiePolicy={'single_host_origin'}
   />
