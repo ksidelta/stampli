590b2f3635ff8e0db4345f44433b46ae0e363752
psuwala <oneat@vp.pl>
2021-06-30 21:48:54 +0200

    backend: fucked upd JWT validation, hope it is secure

--- stampli-backend/src/main/java/com/example/infrastructure/jwt/external/ExternalProvidersConfiguration.java
+++ stampli-backend/src/main/java/com/example/infrastructure/jwt/external/ExternalProvidersConfiguration.java
@@ -4,8 +4,12 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
 import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.oauth2.jwt.JwtValidators;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationProvider;
 
 @Configuration
@@ -14,7 +18,14 @@ public class ExternalProvidersConfiguration {
     @Bean
     @Qualifier
     JwtAuthenticationProvider googleProvider() {
-        return new JwtAuthenticationProvider(JwtDecoders.fromIssuerLocation("accounts.google.com"));
+        NimbusJwtDecoder jwtDecoder = (NimbusJwtDecoder) JwtDecoders.fromIssuerLocation("https://accounts.google.com");
+
+        final var issueProvider = JwtValidators.createDefaultWithIssuer("accounts.google.com");
+        final var validator = new DelegatingOAuth2TokenValidator<>(issueProvider);
+
+        jwtDecoder.setJwtValidator(validator);
+
+        return new JwtAuthenticationProvider(jwtDecoder);
     }
 
     @Bean
