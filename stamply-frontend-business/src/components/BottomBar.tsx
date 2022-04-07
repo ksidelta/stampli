a6cc85b86adb80e709ddfacdb0c08a2f6b8a4cf0
Maciej Błędkowski <23295125+mbledkowski@users.noreply.github.com>
2022-04-07 16:21:04 +0200

    Removed text-decoration from navigation elements in BottomBar

--- stamply-frontend-business/src/components/BottomBar.tsx
+++ stamply-frontend-business/src/components/BottomBar.tsx
@@ -8,6 +8,45 @@ import discount from "../img/discount_black_24dp.svg";
 import options from "../img/menu_black_24dp.svg";
 import {Link} from "react-router-dom";
 
+const StyledA = styled.a`
+  color: black;
+  
+  &&:link {
+    text-decoration: none;
+  }
+
+  &&:visited {
+    text-decoration: none;
+  }
+
+  &&:hover {
+    text-decoration: none;
+  }
+
+  &&:active {
+    text-decoration: none;
+  }
+`
+
+const StyledLink = styled(Link)`
+  color: black;
+  
+  &&:link {
+    text-decoration: none;
+  }
+
+  &&:visited {
+    text-decoration: none;
+  }
+
+  &&:hover {
+    text-decoration: none;
+  }
+
+  &&:active {
+    text-decoration: none;
+  }
+`
 
 const StyledFab = styled(Fab)({
     position: 'absolute',
@@ -29,37 +68,37 @@ export default function BottomBar() {
                 sx={{backgroundColor: 'primary.light', color: '#000', top: 'auto', bottom: 0}}>
             <Toolbar>
                 <Box sx={{flexGrow: 1}}/>
-                <Link to="/menu">
+                <StyledLink to="/menu">
                     <StyledIconButton color="inherit" aria-label="restaurant menu">
                         <img src={menu} alt={"Restaurant menu icon"}/>
                         <Typography variant="button">menu</Typography>
                     </StyledIconButton>
-                </Link>
-                <a href="https://content-media.pl/" target="_blank" referrerPolicy="origin">
+                </StyledLink>
+                <StyledA href="https://content-media.pl/" target="_blank" referrerPolicy="origin">
                     <StyledIconButton color="inherit" aria-label="wifi">
                         <img src={wifi} alt={"WiFi icon"}/>
                         <Typography variant="button">WiFi marketing</Typography>
                     </StyledIconButton>
-                </a>
+                </StyledA>
                 <Box sx={{flexGrow: 1}}/>
-                <Link to="/qr">
+                <StyledLink to="/qr">
                     <StyledFab color="primary" aria-label="scan qr code">
                         <img src={qr} alt={"QR Code icon"}/>
                     </StyledFab>
-                </Link>
+                </StyledLink>
                 <Box sx={{flexGrow: 1}}/>
-                <Link to="/discounts">
+                <StyledLink to="/discounts">
                     <StyledIconButton color="inherit" aria-label="discounts">
                         <img src={discount} alt={"Discount icon"}/>
                         <Typography variant="button">discount</Typography>
                     </StyledIconButton>
-                </Link>
-                <Link to="/options">
-                    <StyledIconButton color="inherit" aria-label="options" href="/options">
+                </StyledLink>
+                <StyledLink to="/options">
+                    <StyledIconButton color="inherit" aria-label="options">
                         <img src={options} alt={"Options icon"}/>
                         <Typography variant="button">options</Typography>
                     </StyledIconButton>
-                </Link>
+                </StyledLink>
                 <Box sx={{flexGrow: 1}}/>
             </Toolbar>
         </AppBar>
