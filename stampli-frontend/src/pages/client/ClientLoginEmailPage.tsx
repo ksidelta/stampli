c4707b57ec324e405a011c46c46f3496c7807880
psuwala <oneat@vp.pl>
2022-04-14 21:40:12 +0200

    added loginService.login()

--- stampli-frontend/src/pages/client/ClientLoginEmailPage.tsx
+++ stampli-frontend/src/pages/client/ClientLoginEmailPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useContext, useMemo, useState } from 'react';
 import styled from 'styled-components';
 import { MobilePage } from '../base/MobilePage';
 import Header from '../../components/Header';
@@ -6,6 +6,10 @@ import Box from '@mui/material/Box';
 import TextField from '../../components/TextField';
 import Button from '../../components/Button';
 import ButtonText from '../../components/ButtonText';
+import { InputState } from '../../state/form/input/InputState';
+import { TextInputStateImpl } from '../../v2/components/simple/form/input/TextInputState';
+import { observer } from 'mobx-react';
+import { InjectionContext } from '../../components/stated/context/InjectionContext';
 
 const Inner = styled.div`
   height: 100vh;
@@ -20,23 +24,27 @@ const Inner = styled.div`
   }
 `;
 
-export default function LoginEmailPage() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  return (
-    <MobilePage>
-      <Inner>
-        <Header title='Zaloguj Się (Email)' />
-        <Box
-          component='form'
-          autoComplete='on'
-        >
-          <TextField text='Adres Email' value={email} setValue={setEmail} required={true} />
-          <TextField text='Hasło' value={password} setValue={setPassword} required={true} />
-          <Button text='Zaloguj Się' link='/stamples' button='' onClick={()=>alert(`${email} ${password}`)} />
-          <ButtonText text='Zarejestruj Się Adresem Email' link='/register/email' />
-        </Box>
-      </Inner>
-    </MobilePage>
-  );
-}
+export default observer(() => {
+    const email = useMemo(() => TextInputStateImpl.create(''), []);
+    const password = useMemo(() => TextInputStateImpl.create(''), []);
+    const mainContext = useContext(InjectionContext);
+
+    return (
+      <MobilePage>
+        <Inner>
+          <Header title='Zaloguj Się (Email)' />
+          <Box
+            component='form'
+            autoComplete='on'
+          >
+            <TextField text='Adres Email' value={email.text()} setValue={t => email.setText(t)} required={true} />
+            <TextField text='Hasło' value={password.text()} setValue={t => password.setText(t)} required={true} />
+            <Button text='Zaloguj Się' link='/stamples' button=''
+                    onClick={() => mainContext.loginService.login(email.text(), password.text())} />
+            <ButtonText text='Zarejestruj Się Adresem Email' link='/register/email' />
+          </Box>
+        </Inner>
+      </MobilePage>
+    );
+  }
+);
