d2666e025ba93a6d8512a999a44d6f23843fda11
Maciej Błędkowski <pub@mble.dk>
2022-05-05 14:23:04 +0200

    Multiple small changes

---
+++ stamply-frontend-business/src/pages/QR.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import QRCode from 'react-qr-code';
+
+export default function QR() {
+    return (
+        <main>
+            <QRCode value="hey" />
+        </main>
+    )
+}
