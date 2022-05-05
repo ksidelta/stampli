a9d86d588209842aca7275a354590169333cd537
Maciej Błędkowski <pub@mble.dk>
2022-05-05 14:29:18 +0200

    Multiple small changes

--- stamply-frontend-business/src/App.tsx
+++ stamply-frontend-business/src/App.tsx
@@ -1,30 +1,29 @@
-import React, { Fragment } from "react";
-import { Route, Link, Routes} from "react-router-dom";
+import React, {Fragment} from "react";
+import {Route, Link, Routes, Navigate} from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 import Menu from './pages/Menu';
 import BottomBar from "./components/BottomBar";
+import QR from "./pages/QR";
+import styled from "styled-components";
 
-
+const Inner = styled.div`
+  min-height: 100vh;
+  display: grid;
+  grid-template-rows: 1fr auto;
+`
 
 export default function App() {
     return (
-        <>
-        <main>
-            <nav>
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/about/E">About</Link></li>
-                    <li><Link to="/contact">Contact</Link></li>
-                </ul>
-            </nav>
-        </main>
-    <Routes>
-        <Route path="/" element={<Menu/>} />
-        <Route path="/menu" element={() => <h1>Welcome!</h1>} />
-        <Route path="/contact" element={<img src={logo}/>} />
-    </Routes>
+        <Inner>
+            <Routes>
+                <Route path="/" element={<Navigate to="/menu"/>}/>
+                <Route path="/menu" element={<Menu/>}/>
+                <Route path="/qr" element={<QR/>}/>
+                <Route path="/discounts" element={<img src={logo}/>}/>
+                <Route path="/options" element={<img src={logo}/>}/>
+            </Routes>
             <BottomBar/>
-    </>
+        </Inner>
     );
 }
