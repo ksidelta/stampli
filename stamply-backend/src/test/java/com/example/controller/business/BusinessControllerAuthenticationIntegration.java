d7ea689d74de76954a58c93bebf4ee9cd13cc1c7
Marcin Barzyk <mbarzyk@atlassian.com>
2022-06-18 20:34:03 +0200

    ISSUE-none: First attempt to spring boot the class

--- stamply-backend/src/test/java/com/example/controller/business/BusinessControllerAuthenticationIntegration.java
+++ stamply-backend/src/test/java/com/example/controller/business/BusinessControllerAuthenticationIntegration.java
@@ -1,37 +1,42 @@
 package com.example.controller.business;
 
-import com.example.BaseTestConfiguration;
+import com.example.SpringBoot;
+import com.example.common.BaseSpringBootIT;
 import com.example.infrastructure.jwt.generator.TokenGenerator;
 import com.example.modules.authentication.domain.user.entity.AbstractUserAggregate;
 import com.example.modules.business.controller.BusinessController;
+import com.example.modules.business.service.BusinessDto;
 import com.example.modules.business.service.BusinessService;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
-import org.springframework.test.web.servlet.MockMvc;
 
 import javax.servlet.Filter;
 import java.util.Arrays;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.verify;
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
+@SpringBootTest(classes = SpringBoot.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@EnableAutoConfiguration(exclude = {HibernateJpaAutoConfiguration.class})
 @ExtendWith(SpringExtension.class)
-@ExtendWith(MockitoExtension.class)
-@SpringJUnitWebConfig({BaseTestConfiguration.class})
-public class BusinessControllerAuthenticationIntegration {
-    protected MockMvc mockMvc;
-
+public class BusinessControllerAuthenticationIntegration extends BaseSpringBootIT {
     @Mock
     protected BusinessService businessService;
 
@@ -44,35 +49,40 @@ public class BusinessControllerAuthenticationIntegration {
     @Autowired
     protected Filter springSecurityFilterChain;
 
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    final HttpHeaders headers = new HttpHeaders();
+
+    @BeforeAll
+    static public void start() {
+        startMariaDBWithLoggingForClass(BusinessControllerAuthenticationIntegration.class);
+    }
+
     @BeforeEach
-    public void setUp() {
-        mockMvc = standaloneSetup(businessController)
-                .apply(springSecurity(springSecurityFilterChain))
-                .build();
+    public void setUpHeaders() {
+        headers.add("Authorization", "Bearer " + createToken());
     }
 
     @Test
-    public void givenExistingUserWhenBusinessIsCreatedThenServiceIsExecutedWithUserId() throws Exception {
-        final var mvcResult = mockMvc.perform(
-                post("/api/business/")
-                        .accept(MediaType.APPLICATION_JSON)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .header("Authorization", "Bearer " + createToken())
-        ).andExpect(status().is(201)).andReturn();
+    public void givenExistingUserWhenBusinessIsCreatedThenServiceIsExecutedWithUserId() {
+        final HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Bearer " + createToken());
 
+        final var response = restTemplate.exchange("/api/business", HttpMethod.POST, new HttpEntity<>(headers), BusinessDto.class);
+
+        assertThat(response.getStatusCode().value(), is(201));
         verify(businessService).createBusiness(1);
     }
 
     @Test
-    public void givenUnauthenticatedUserWhenBusinessIsCreatedThen401IsReturned() throws Exception {
-        final var mvcResult = mockMvc.perform(
-                post("/api/business/")
-                        .accept(MediaType.APPLICATION_JSON)
-                        .contentType(MediaType.APPLICATION_JSON)
-        ).andExpect(status().is(401)).andReturn();
+    public void givenUnauthenticatedUserWhenBusinessIsCreatedThen401IsReturned() {
+        final var response = restTemplate.exchange("/api/business", HttpMethod.POST, new HttpEntity<>(new HttpHeaders()), BusinessDto.class);
+
+        assertThat(response.getStatusCode().value(), is(401));
     }
 
-    public String createToken() {
+        public String createToken() {
         final var user = new AbstractUserAggregate();
         user.setRoles(Arrays.asList());
         user.setId(1);
